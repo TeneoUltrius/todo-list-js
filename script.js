5e1e4824bcc1f2430cd8fcc7ce37612bf56fc33c
TeneoUltrius <teneoultrius@gmail.com>
2022-05-29 17:57:04 +0200

    Main scripts and appearance done

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
-
-
-let input = document.getElementById('item-input');
+let itemsCounter = 0; //For appear clearAll button
 
 function AddItem() {
-    let newItem = document.getElementById('item-input').value;
-    let itemDiv = document.createElement('div');
-    itemDiv.classList.add('item');
-    itemDiv.innerHTML = `
-            <button class="btn-done" type="button">DONE</button>
-            <span class="item-text" >${newItem}</span>`;
-    document.getElementById('todo-items').append(itemDiv);
 
-    addListenerDoneButtons();
+    if (document.getElementById('item-input').value.length == 0) {
+        document.getElementById('item-input').setAttribute('placeholder', 'Type your text here');
+    } else {
+        document.getElementById('item-input').setAttribute('placeholder', '')    
+
+        let input = document.getElementById('item-input');
+        let newItem = document.getElementById('item-input').value;
+        let itemDiv = document.createElement('div');
+        itemDiv.classList.add('item');
+        itemDiv.innerHTML = `
+                <button class="btn-done" type="button">DONE</button>
+                <span class="item-text" >${newItem}</span>`;
+
+        document.getElementById('todo-items').append(itemDiv);
+
+        addListenerDoneButtons();
+        clearButtonVisible();
+        clearText();  
+    }
 };
 
 function addListenerDoneButtons() {
     let doneButtons = document.getElementsByClassName('btn-done');
-    console.log(doneButtons);
 
-
-    for(i = 0; i < doneButtons.length; i++) {
+    for(let i = 0; i < doneButtons.length; i++) {
         let button = doneButtons[i];
         button.addEventListener('click', function(event) {
             let buttonClicked = event.target;
-        buttonClicked.parentElement.classList.add('item-done');    
+            buttonClicked.parentElement.classList.add('item-done');   
         });
     }
 }
 
-function clearAllItems() {
-    let item = document.getElementsByClassName('btn-done');
+function createButtonClearAll () {
+    let clearButtonText = "CLEAR";
+    let createButton = document.createElement('button');
 
-    console.log('item ' + item);
+    document.getElementById('container').append(createButton);
+    createButton.setAttribute('id', 'clearAll');
+    createButton.setAttribute('onclick', 'clearAllItems()');
+    createButton.setAttribute('type', 'button');
+    createButton.innerHTML = clearButtonText;
+}
 
-    for (i = 0; i < item.length; i++) {
-        let button = item[i];
-        console.log(button);
-        button.addEventListener('click', function (event) {
-            let buttonClicked = event.target;
-            buttonClicked.parentElement.remove();
-        });
+function clearButtonVisible () {
+    let showClearButton = document.getElementById('clearAll').classList.remove('invisible');
+
+    if (itemsCounter == 0) {
+        showClearButton;
+    }
+}
+
+function clearAll () {
+    let items = document.getElementsByClassName('item');
+
+    items[0].remove();
+    while (items.length > 0) {
+        clearAll();
     }
 
+    if (items.length == 0) {
+        document.getElementById('clearAll').classList.add('invisible');
+    }
+    itemsCounter = items.length;
 }
+
+function clearText () {
+    document.getElementById('item-input').value = "";
+}
+
