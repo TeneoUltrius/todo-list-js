57e1cff7d8c9a3f0e1cdf4d1050af10946177387
TeneoUltrius <teneoultrius@gmail.com>
2023-03-08 17:07:03 +0100

    Review v2

--- script.js
+++ script.js
@@ -1,58 +1,51 @@
-let itemsCounter = 0; //For appear clearAll button
+// Listen to button click on "Return"
+document.getElementById("item-input").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("addBtn").click();
+    }
+});
 
+// Add new task
 function addItem() {
-
-    if (document.getElementById('item-input').value.length == 0) {
-        document.getElementById('item-input').setAttribute('placeholder', 'Type your text here');
+    let itemInput = document.getElementById('item-input');
+    if (itemInput.value.length == 0) {
+        itemInput.classList.add('required');
+        itemInput.focus();
     } else {
-        document.getElementById('item-input').setAttribute('placeholder', '')    
-
+        itemInput.classList.remove('required');
         let newItem = document.getElementById('item-input').value;
         let itemDiv = document.createElement('div');
         itemDiv.classList.add('item');
         itemDiv.innerHTML = `
-                <div class="item-text"><span>${newItem}</span></div>
-                <button class="btn-done" type="button"><i class="icofont-verification-check"></i></button>`;
-
+            <div class="item-text"><span>${newItem}</span></div>
+            <button id="btn-done" class="btn-done" type="button" onclick="itemDone()"><i class="icofont-verification-check"></i></button>`;
         document.getElementById('todo-items').append(itemDiv);
-
-        addListenerDoneButtons();
         clearButtonVisible();
         clearText();  
     }
-};
+}
 
-function addListenerDoneButtons() {
-    let doneButtons = document.getElementsByClassName('btn-done');
-
-    for(let i = 0; i < doneButtons.length; i++) {
-        let button = doneButtons[i];
-        button.addEventListener('click', function(event) {
-            let buttonClicked = event.target;
-            buttonClicked.parentElement.classList.add('item-done');   
-        });
+// Task switch to done
+function itemDone() {
+    if (event.target.closest("div").classList.contains('item-done')) {
+        event.target.closest("div").classList.remove('item-done');
+        event.target.closest("#btn-done").innerHTML = `
+            <i class="icofont-verification-check"></i>`;
+    } else {
+        event.target.closest("div").classList.add('item-done');
+        event.target.closest("#btn-done").innerHTML = `
+            <i class="icofont-redo"></i>`;
     }
 }
 
-function createButtonClearAll () {
-    let clearButtonText = "CLEAR";
-    let createButton = document.createElement('button');
 
-    document.getElementById('container').append(createButton);
-    createButton.setAttribute('id', 'clearAll');
-    createButton.setAttribute('onclick', 'clearAllItems()');
-    createButton.setAttribute('type', 'button');
-    createButton.innerHTML = clearButtonText;
-}
-
+// Show or hide clear all button
 function clearButtonVisible () {
-    let showClearButton = document.getElementById('clearAll').classList.remove('invisible');
-
-    if (itemsCounter == 0) {
-        showClearButton;
-    }
+    document.getElementById('clearAll').closest("section").classList.remove('hide');
 }
 
+// Remove all tasks
 function clearAll () {
     let items = document.getElementsByClassName('item');
 
@@ -62,12 +55,19 @@ function clearAll () {
     }
 
     if (items.length == 0) {
-        document.getElementById('clearAll').classList.add('invisible');
+        document.getElementById('clearAll').closest("section").classList.add('hide');
     }
-    itemsCounter = items.length;
 }
 
+// Clear input text
 function clearText () {
     document.getElementById('item-input').value = "";
 }
 
+// Dynamic font-size
+function sizeFont() {
+    let x = document.getElementById("item-input");
+    let initialSize = 1.2 - x.value.length/200;
+    initialSize = initialSize <= 1 ? 0.9 : initialSize;
+    x.style.fontSize = initialSize + "em";
+}
