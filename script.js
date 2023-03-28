d820a70f0a3df5eb11b9a9bc1db3ecbdf020d7a7
TeneoUltrius <teneoultrius@gmail.com>
2023-03-28 12:38:12 +0200

    Update script.js
    Overall refactoring

--- script.js
+++ script.js
@@ -1,73 +1,71 @@
-// Listen to button click on "Return"
-document.getElementById("item-input").addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("addBtn").click();
-    }
+// Add event listener for the "Return" key
+const itemInput = document.getElementById("item-input");
+itemInput.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  if (event.key === "Enter") {
+    addItem();
+  }
 });
 
 // Add new task
 function addItem() {
-    let itemInput = document.getElementById('item-input');
-    if (itemInput.value.length == 0) {
-        itemInput.classList.add('required');
-        itemInput.focus();
-    } else {
-        itemInput.classList.remove('required');
-        let newItem = document.getElementById('item-input').value;
-        let itemDiv = document.createElement('div');
-        itemDiv.classList.add('item');
-        itemDiv.innerHTML = `
-            <div class="item-text"><span>${newItem}</span></div>
-            <button id="btn-done" class="btn-done" type="button" onclick="itemDone()"><i class="icofont-verification-check"></i></button>`;
-        document.getElementById('todo-items').append(itemDiv);
-        clearButtonVisible();
-        clearText();  
-    }
+  const itemInput = document.getElementById("item-input");
+  const newItem = itemInput.value.trim();
+  if (newItem.length === 0) {
+    itemInput.classList.add("required");
+    itemInput.focus();
+    return;
+  }
+  itemInput.classList.remove("required");
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
+  itemDiv.innerHTML = `
+    <div class="item-text"><span>${newItem}</span></div>
+    <button class="btn-done" type="button"><i class="icofont-verification-check"></i></button>`;
+  const btnDone = itemDiv.querySelector(".btn-done");
+  btnDone.addEventListener("click", () => {
+    itemDone(btnDone);
+  });
+  document.getElementById("todo-items").appendChild(itemDiv);
+  clearButtonVisible();
+  clearText();
 }
 
 // Task switch to done
-function itemDone() {
-    if (event.target.closest("div").classList.contains('item-done')) {
-        event.target.closest("div").classList.remove('item-done');
-        event.target.closest("#btn-done").innerHTML = `
-            <i class="icofont-verification-check"></i>`;
-    } else {
-        event.target.closest("div").classList.add('item-done');
-        event.target.closest("#btn-done").innerHTML = `
-            <i class="icofont-redo"></i>`;
-    }
+function itemDone(btnDone) {
+  const itemDiv = btnDone.closest(".item");
+  itemDiv.classList.toggle("item-done");
+  const icon = btnDone.querySelector("i");
+  icon.classList.toggle("icofont-verification-check");
+  icon.classList.toggle("icofont-redo");
 }
 
-
 // Show or hide clear all button
-function clearButtonVisible () {
-    document.getElementById('clearAll').closest("section").classList.remove('hide');
+function clearButtonVisible() {
+  const clearAll = document.getElementById("clearAll");
+  const todoItems = document.getElementById("todo-items");
+  if (todoItems.children.length > 0) {
+    clearAll.closest("section").classList.remove("hide");
+  } else {
+    clearAll.closest("section").classList.add("hide");
+  }
 }
 
 // Remove all tasks
-function clearAll () {
-    let items = document.getElementsByClassName('item');
-
-    items[0].remove();
-    while (items.length > 0) {
-        clearAll();
-    }
-
-    if (items.length == 0) {
-        document.getElementById('clearAll').closest("section").classList.add('hide');
-    }
+function clearAll() {
+  const todoItems = document.getElementById("todo-items");
+  todoItems.innerHTML = "";
+  clearButtonVisible();
 }
 
 // Clear input text
-function clearText () {
-    document.getElementById('item-input').value = "";
+function clearText() {
+  document.getElementById("item-input").value = "";
 }
 
 // Dynamic font-size
 function sizeFont() {
-    let x = document.getElementById("item-input");
-    let initialSize = 1.2 - x.value.length/200;
-    initialSize = initialSize <= 1 ? 0.9 : initialSize;
-    x.style.fontSize = initialSize + "em";
+  const itemInput = document.getElementById("item-input");
+  const initialSize = 1.2 - itemInput.value.length / 200;
+  itemInput.style.fontSize = Math.max(initialSize, 0.9) + "em";
 }
